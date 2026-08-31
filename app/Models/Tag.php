5bb4e0b915bac82b93276fe43abc090332384cb3
ai mizuno <cookiemonster03121989@gmail.com>
2026-08-31 21:26:55 +0900

    feat:モデル作成とリレーション定義 #2

---
+++ app/Models/Tag.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Tag extends Model
+{
+    use HasFactory;
+
+    protected $fillable=[
+        'name',
+    ];
+
+    /** このタグを所有する問い合わせを取得する */
+
+    public function contacts()
+    {
+        return $this->belongsToMany(Contact::class);
+    }
+    
+}
